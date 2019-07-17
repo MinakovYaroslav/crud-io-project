9e32e0362af1e5cd0501ec17609bed0ade0a3e9e
Yaroslav Minakov <minakov.yaroslav92@gmail.com>
2019-07-17 20:22:20 +0300

    Add AbstractIdentifiable class

---
+++ src/model/base/AbstractIdentifiable.java
@@ -0,0 +1,18 @@
+package model.base;
+
+public class AbstractIdentifiable {
+
+    public AbstractIdentifiable(Long id) {
+        this.id = id;
+    }
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}

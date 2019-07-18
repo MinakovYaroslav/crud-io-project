0d11fc0246f83c0c1794dcb5bb69216de2b47c00
Yaroslav Minakov <minakov.yaroslav92@gmail.com>
2019-07-18 18:27:15 +0300

    Add IOUtil class

---
+++ src/io/IOUtil.java
@@ -0,0 +1,42 @@
+package io;
+
+import tools.FilePath;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class IOUtil {
+
+    public static List<String[]> read(String path) {
+        List<String[]> records = new ArrayList<>();
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(path))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                records.add(line.split(";"));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return records;
+    }
+
+    public static void write(List<String[]> data, String path) {
+        try {
+            Path p = Paths.get(path);
+            Path tempFile = Files.createTempFile(p.getParent(), "temp", ".txt");
+            try (BufferedWriter writer = Files.newBufferedWriter(tempFile)) {
+                for (String[] fields : data) {
+                    writer.write(String.join(";", fields));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}

9cd81cbb84094076d1872b76547f75b4e6493ad8
Yaroslav Minakov <minakov.yaroslav92@gmail.com>
2019-07-19 15:21:52 +0300

    Moved to new package Optimized imports

---
+++ src/main/java/com/minakov/crudioproject/repository/CustomerRepository.java
@@ -0,0 +1,6 @@
+package com.minakov.crudioproject.repository;
+
+import com.minakov.crudioproject.model.Customer;
+
+public interface CustomerRepository extends GenericRepository<Customer, Long> {
+}

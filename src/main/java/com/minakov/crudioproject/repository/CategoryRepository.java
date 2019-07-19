41df2448a9a9fc6611b809510e07192184ed8b29
Yaroslav Minakov <minakov.yaroslav92@gmail.com>
2019-07-19 15:21:17 +0300

    Moved to new package Optimized imports

---
+++ src/main/java/com/minakov/crudioproject/repository/CategoryRepository.java
@@ -0,0 +1,6 @@
+package com.minakov.crudioproject.repository;
+
+import com.minakov.crudioproject.model.Category;
+
+public interface CategoryRepository extends GenericRepository<Category, Long> {
+}

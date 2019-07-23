71f51747ad15b0abb8518b69d0c0fa7214f3530d
Yaroslav Minakov <minakov.yaroslav92@gmail.com>
2019-07-24 00:53:33 +0300

    Add catch block in read() method Fixed issue in write() method

--- src/main/java/com/minakov/crudioproject/io/IOUtil.java
+++ src/main/java/com/minakov/crudioproject/io/IOUtil.java
@@ -3,8 +3,8 @@ package com.minakov.crudioproject.io;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,6 +14,7 @@ public class IOUtil {
     public static List<String[]> read(String path) {
         try {
             return Files.lines(Paths.get(path)).map(l -> l.split(";")).collect(Collectors.toList());
+        } catch (NoSuchFileException ignore) {
         } catch (IOException e) {
             System.err.println("Reading file error: " + e);
         }
@@ -23,8 +24,8 @@ public class IOUtil {
     public static void write(List<String[]> data, String path) {
         try {
             Files.write(Paths.get(path), data.stream()
-                    .map(f -> String.join(";", f))
-                    .collect(Collectors.toList()), StandardCharsets.UTF_8, StandardOpenOption.CREATE);
+                    .map(fields -> String.join(";", fields))
+                    .collect(Collectors.toList()), StandardCharsets.UTF_8);
         } catch (IOException e) {
             System.err.println("Writing file error: " + e);
         }
